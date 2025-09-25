a56c363c814bfa4391baefcd9f1db23c5d353984
Sasha Dunaizev <jenderprolite@gmail.com>
2025-09-25 19:51:22 +0500

    Task2

---
+++ Tyuiu.DunaizevAO.Sprint1.Task1.V4/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.DunaizevAO.Sprint1.Task1.V4.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Дунайцев А. О. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Организация ввода\\вывода в консольных приложениях                *");
+Console.WriteLine("* Задание #1                                                              *");
+Console.WriteLine("* Вариант #4                                                              *");
+Console.WriteLine("* Выполнил: Дунайцев А. О. | РППб-25-1                                    *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* вычисляет результат по формуле (x+y)/x*y и печатает его на экране.      *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+double x, y;
+
+Console.WriteLine("Введите значение X:");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y:");
+y = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.Calculate(x, y));
+
+Console.ReadLine();
